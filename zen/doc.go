44e99811f249829255b4897ae91044747ff6549c
Joe <joecolasurdo@eltoro.com>
2018-02-25 12:42:23 -0800

    Adding create command and examples to doc file.

--- zen/doc.go
+++ zen/doc.go
@@ -10,15 +10,34 @@ DESCRIPTION
     zen is a small utility for interacting with ZenHub boards through a simple command line interface.
 
 COMMANDS
-    close <issue>                 Changes the status of the specified issue to closed.
-    drop <issue>                  Removes you as an assignee on the specified issue.
-    list [parameters]             Lists all of the pipelines and issues for the current repository.
+    close <issue>                    Changes the status of the specified issue to closed.
+    create "<title>" as <pipeline>   Creates a new issue in the specified pipeline.
+                                     The issue title must be surrounded by double quotes.
+    drop <issue>                     Removes you as an assignee on the specified issue.
+    list [parameters]                Lists all of the pipelines and issues for the current repository.
         parameters:
-        [backlog]                 The backlog pipeline is omitted from results unless "backlog" is supplied.
-        [only <login>|me]         The list of issues will be filtered to only include the specified github login.
-                                  When this option is supplied, unassigned issues are still displayed.
-                                  If "me" is supplied as the login, the current authenticated user's login is used.
-    move <issue> [to] <pipeline>  Moves the specified issue from its current pipeline to the specified pipeline.
-    open <issue>                  Changes the status of the specified issue to open.
-    pick up <issue>               Adds you as the assignee on the specified issue and removes any other assignees.
+        [backlog]                    The backlog pipeline is omitted from results unless "backlog" is supplied.
+        [only <login>|me]            The list of issues will be filtered to only include the specified github login.
+                                     When this option is supplied, unassigned issues are still displayed.
+                                     If "me" is supplied as the login, the current authenticated user's login is used.
+    move <issue> [to] <pipeline>     Moves the specified issue from its current pipeline to the specified pipeline.
+    open <issue>                     Changes the status of the specified issue to open.
+    pick up <issue>                  Adds you as the assignee on the specified issue and removes any other assignees.
+
+EXAMPLES
+    To close an issue number 123:
+        
+        $ zen close 123
+
+    To create a new issue in the 'prioritized' pipeline:
+
+        $ zen create "There's clearly a bug in this code" as prioritized
+
+    To list only only my issues:
+
+        $ zen list only me
+
+    To move issue 999 to the 'in progress' pipeline:
+
+        $ zen move 999 to in progress
 `
